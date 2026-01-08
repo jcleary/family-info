0ca95ff9eead376e83e255537a8f30d36fbc79a1
John Cleary <johnmartincleary@gmail.com>
2026-01-08 18:19:07 +0000

    update restaurants

--- lib/restaurants.rb
+++ lib/restaurants.rb
@@ -1,22 +1,21 @@
 class Restaurants
-  attr_reader :config
+  attr_reader :config, :open, :closed
 
   def initialize(config)
     @config = config
-  end
-
-  def open_today
     @open = []
+    @closed = []
+
     today = Date.today
     dow_words = today.strftime("%A").downcase
 
     config.restaurants.each do |restaurant|
       opening_hours = Array(restaurant[:opening_hours][dow_words])
-      unless opening_hours.include?("Closed")
+      if opening_hours.include?("Closed")
+        @closed << { name: restaurant[:name], opening_hours: opening_hours }
+      else
         @open << { name: restaurant[:name], opening_hours: opening_hours }
       end
     end
-
-    @open
   end
 end
